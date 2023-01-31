50c8c37767298b951baaadafcd8f03971f5393c8
LesterCuasay <lestercuasay@live.co.uk>
2023-01-31 10:10:45 +0000

    Added to run_game function

--- run.py
+++ run.py
@@ -13,6 +13,48 @@ def run_game(word):
     guessed_letters = []
     guessed_words = []
     tries = 6
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or a word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congrats, You guessed the word! You win")
+    else:
+        print("Sorry, no more tries. The correct word was " + word + "Better luck next time!")
 
 
 def display_hangman(tries):
@@ -32,7 +74,7 @@ def display_hangman(tries):
             |      O
             |     \\|/
             |      |
-            |     / 
+            |     /
             -
         """
         """
@@ -41,7 +83,7 @@ def display_hangman(tries):
             |      O
             |     \\|/
             |      |
-            |     
+            |
             -
         """
         """
@@ -50,7 +92,7 @@ def display_hangman(tries):
             |      O
             |     \\|
             |      |
-            |     
+            |
             -
         """
         """
@@ -59,16 +101,16 @@ def display_hangman(tries):
             |      O
             |      |
             |      |
-            |     
+            |
             -
         """
         """
             --------
             |      |
             |      O
-            |      
-            |      
-            |     
+            |
+            |
+            |
             -
         """
     ]
